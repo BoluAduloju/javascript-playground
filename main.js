0d527ed48f0a6ad178d94693a9c59fd917ac502f
BoluAduloju <boluwatifeaduloju2@gmail.com>
2024-08-20 22:28:25 +0100

    02/ number generator

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-const counterLabel = document.getElementById("counterLabel");
-const increaseBtn = document.getElementById("increaseBtn");
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-let count = 0;
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
 
 
-increaseBtn.onclick = function(){
-    count++;
-    counterLabel.textContent = count; 
-}
-decreaseBtn.onclick = function(){
-    count--;
-    counterLabel.textContent = count; 
-}
-resetBtn.onclick = function(){
-    count = 0;
-    counterLabel.textContent = count; 
-}
+
+
+
+
+
