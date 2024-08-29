3779bf6a2ae337072be23d8db11b0705abb508e4
BoluAduloju <boluwatifeaduloju2@gmail.com>
2024-08-29 15:03:13 +0100

    added functionality

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
+const apikey = "46f80a02ecae410460d59960ded6e1c6";
+const weatherDataEl = documnet.getElementById("weather-data");
+const cityInputEl = documnet.getElemntById("city-input");
+const formEl = document.querySelector("form");
 
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    console.log(city.Value);
+    getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue){
+    try{
+       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
+       if(!response.ok){
+        throw new Error("Network response was not ok")
+       }
+
+       const data = await response.json()
+
+       console.log(data);
+    } catch(error){
+
+    }
+}
 
